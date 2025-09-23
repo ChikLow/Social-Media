8b376c2b9213c1af7f7110564d6036341fd9f54a
Roman <romanshtanhret@gmail.com>
2025-09-23 21:29:55 +0300

    did account system etc

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from .forms import CustomUserCreationForm, UserProfileForm, LoginForm
+
 
 # Create your views here.
+class RegisterView(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'accounts/edit_profile.html'
+    form_class = UserProfileForm
+    success_url = reverse_lazy('profile')
+
+    def get_object(self):
+        return self.request.user
+    
+class UserLoginView(LoginView):
+    template_name = 'auth_system/login.html'
+    form_class = LoginForm

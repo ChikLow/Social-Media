cb6d067f9e884012e988ea1d9edf84d8d96b69a8
Roman <romanshtanhret@gmail.com>
2025-11-11 20:17:17 +0200

    додав sidebar, footer

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import DetailView
 from django.urls import reverse_lazy,reverse
+from django.shortcuts import redirect
 from django.contrib.auth.views import LoginView
 from .forms import CustomUserCreationForm, UserProfileForm, LoginForm
+from accounts.models import User
 
 
 
@@ -28,3 +31,16 @@ class UserLoginView(LoginView):
     def get_success_url(self):
         
         return reverse('profile_detail', args=[self.request.user.id])
+    
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'accounts/profile.html'
+    context_object_name = 'profile_user'
+    login_url = 'login'  # якщо не залогінений — перекине сюди
+
+    def get(self, request, *args, **kwargs):
+        # якщо користувач намагається відкрити не свій профіль
+        if request.user.id != int(self.kwargs['pk']):
+            return redirect('profile', pk=request.user.id)
+        return super().get(request, *args, **kwargs)

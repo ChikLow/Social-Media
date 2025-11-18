8c6ebcfe2b1da8d70ffa7920ee98633c28e2af58
Roman <romanshtanhret@gmail.com>
2025-11-18 20:37:24 +0200

    підготував проєкт під production

--- social_media/settings.py
+++ social_media/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w6i*1v8ffme7id6du_n&-l8!^n^w48l+jb$drrohvhhykk-#(6'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
